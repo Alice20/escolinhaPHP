8062f405a969c67510e8f11911cc551892ffe7ba
alice20 <alii.alice@hotmail.com>
2015-11-18 21:39:53 -0200

    alterado

---
+++ receberPOST.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Receber dados através de POST</title>
+    </head>
+    <body>
+        <h1>Exemplo de recebimento - Método POST</h1>
+
+        Bem-Vindo <?php echo $_POST["name"]; ?><br>
+        Seu endereço de e-mail é: <?php echo $_POST["email"]; ?>
+
+
+    </body>
+</html>
