55acacdafc7857d26ca4bad597e749376d3d8b1f
alice20 <alii.alice@hotmail.com>
2015-11-18 21:57:26 -0200

    alterado

--- index.php
+++ index.php
@@ -8,16 +8,29 @@
         <h1>Manuseio de Formulário com PHP</h1>
         <h2>Referências:</h2>
         <ul>
-            
+
             <li><a href="http://www.w3schools.com/php">
                     W3Schools</a></li>
-            
+
             <li><a href="http://php.net/">
                     Manual do PHP</a></li>
-            
+
         </ul>
-        <?php
+
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
         
+        Name: <input type="text" name="name"><br />
+        E-mail: <input type="text" name="email"><br />
+        Website: <input type="text" name="website"><br />
+        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+
+        Gender:<br />
+        <input type="radio" name="gender" value="female">Female
+        <input type="radio" name="gender" value="male">Male
+        <br />
+        
+        <input type="submit" value="Enviar";
+        <?php
         // put your code here
         ?>
     </body>
