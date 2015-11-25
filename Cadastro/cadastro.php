a17da7523444721bc72a78dd1af054573efad2db
alice20 <alii.alice@hotmail.com>
2015-11-25 20:27:38 -0200

    banco de dados funcionando

--- Cadastro/cadastro.php
+++ Cadastro/cadastro.php
@@ -1,28 +1,24 @@
 <?php
-require_once 'dbconfig.php';
- 
-if (isset($_POST['btn'])){
 
-/** Conexão com o banco de dados */
+if (isset($_POST['btn'])) {
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=escolinha", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
+    require_once 'dbconfig.php';
+
+    /** Conexão com o banco de dados */
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=escolinha", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-    
-    
-    /**Recepçao de dados */
-    
+
+
+    /*     * Recepçao de dados */
+
     echo "<h1> $_POST[email] </h1>";
-} else{
+} else {
     //Botão cadastrar nao foi pressionado
     //Redicionar para a pagina inicial
     header('Location: index.php');
-    
-}
+} 
