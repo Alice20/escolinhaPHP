3e68ca88e69d3eb8e6eb99eaa7c0bb0616b614a4
alice20 <alii.alice@hotmail.com>
2015-11-25 22:03:09 -0200

    inserte funcionando

--- Cadastro/cadastro.php
+++ Cadastro/cadastro.php
@@ -1,54 +1,70 @@
 <?php
-function gerarCodigo(){
+
+//Faz a requisição de dados paraconexão com o BD
+require_once 'dbconfig.php';
+/*
+ * Conexão com o banco de dados 
+ */
+try {//Criação do objeto $conn - conexão
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    //echo "Conectado ao banco $dbname em $host com sucesso.";
+} catch (PDOException $pe) {
+    die("Não foi possível se conectar ao banco $dbname :" . $pe->getMessage());
+}
+
+function gerarCodigo() {
     return sha1(mt_rand());
 }
+
 /**
  * Verifica se o botão cadastrar foi pressionado
  * 
  */
 if (isset($_POST['btn'])) {
-    //Faz a requisição de dados paraconexão com o BD
-    require_once 'dbconfig.php';
-    /*
-     * Conexão com o banco de dados 
-     */
-    try {//Criação do objeto $conn - conexão
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-        //echo "Conectado ao banco $dbname em $host com sucesso.";
-    } catch (PDOException $pe) {
-        die("Não foi possível se conectar ao banco $dbname :" . $pe->getMessage());
-    }
+
     /**
      * Recepção de dados
      */
     if (isset($_POST['email']) && !empty($_POST['email'])) {
         //Filtragem de entrada ded dados
         //$email = $_POST['email']; //Não é correto
-        
-        $email = filter_input(INPUT_POST, 'email',FILTER_SANITIZE_EMAIL);
+
+        $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
         $cod = gerarCodigo();
-        
+
         //String SQL
         $sql = "INSERT INTO lista(email,cod,dtCadastro) "
                 . "values(:email,:cod,now())";
-        $parametros = array(':email'=>$email,
-                            ':cod'=>$cod);
+        $parametros = array(':email' => $email,
+            ':cod' => $cod);
         $p = $conn->prepare($sql);
         $q = $p->execute($parametros);
-        
+
         /**
          * Tarefa de casa
          * Criar um e-mail HTML, enviando um link
          * com o código, para a pessoa clicar
          * e confirmar seu e-mail
          */
-        
-        
     } else {
         header('Location: index.php');
     }
-//Fecha conexão com o banco
+
     $conn = null;
+} elseif (isset($_GET['cod'])) {
+
+    $sql = "SELECT email,cod,situacao,dtCadastro,dtAtualizacao from lista";
+
+    $q = $conn->query($sql);
+    $q->setFetchMode(PDO:: FETCH_ASSOC);
+
+
+    while ($r = $q->fetch()) {
+        eccho "<p>";
+        echo $r 
+                
+    }
+    }
 } else {
     //Botão cadastrar não foi pressionado
     //Redireciona para a página inicial
